fdc636cf9447cc61ab500d636a8115aaedcf9cab
Coderp <58617892+Codedemons@users.noreply.github.com>
2023-06-28 22:30:55 -0700

    Obtencion de directrorio y listador por medio de lo ingresado

--- Organizador/Organizador.py
+++ Organizador/Organizador.py
@@ -1,5 +1,15 @@
 import os
 
-nombre  = "Version"
-print("Primeras pruebas")
-os.mkdir(nombre)
+def listar_directorio(directorio):
+    archivos = os.listdir(directorio)
+    for archivo in archivos:
+        ruta = os.path.join(directorio, archivo)
+        if os.path.isdir(ruta):
+            print(f"Directorio: {ruta}")
+        else:
+            print(f"Archivo: {ruta}")
+
+# Solicitar al usuario que ingrese un directorio
+directorio_ingresado = input("Ingrese la ruta de un directorio: ")
+# Llamar a la función para listar el contenido del directorio
+listar_directorio(directorio_ingresado)
