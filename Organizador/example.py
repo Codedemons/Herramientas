6cc19c8d9704e4b315c6733f7cb5f6a041bea16f
Coderp <58617892+Codedemons@users.noreply.github.com>
2023-06-28 23:46:04 -0700

    Cambiando orientacion de los componentes y mostrando solo archivos

--- Organizador/example.py
+++ Organizador/example.py
@@ -2,16 +2,14 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 
-def listar_directorio():
+def listar_archivos():
     directorio = filedialog.askdirectory()
     if directorio:
         archivos = os.listdir(directorio)
         for archivo in archivos:
             ruta = os.path.join(directorio, archivo)
-            if os.path.isdir(ruta):
-                lista_directorios.insert(tk.END, f"Directorio: {ruta}")
-            else:
-                lista_directorios.insert(tk.END, f"Archivo: {ruta}")
+            if os.path.isfile(ruta):
+                lista_archivos.insert(tk.END, f"Archivo: {ruta}")
 
 # Crear una ventana de Tkinter con resolución 640x360
 ventana = tk.Tk()
@@ -21,22 +19,19 @@ ventana.title("Organizador")
 # Configurar el tamaño de las filas y columnas
 ventana.rowconfigure(0, weight=1)
 ventana.rowconfigure(1, weight=1)
-ventana.rowconfigure(2, weight=1)
-ventana.rowconfigure(3, weight=1)
 ventana.columnconfigure(0, weight=1)
-ventana.columnconfigure(1, weight=1)
 
-# Crear el botón en la primera columna y abarcar 2 filas en posición vertical
-boton = tk.Button(ventana, text="Seleccionar directorio", command=listar_directorio)
-boton.grid(row=1, column=0, rowspan=2, sticky="nsew")
+# Crear el botón en la primera fila
+boton = tk.Button(ventana, text="Seleccionar directorio", command=listar_archivos)
+boton.grid(row=0, column=0, sticky="nsew")
 
-# Crear el Listbox en la segunda columna
-lista_directorios = tk.Listbox(ventana)
-lista_directorios.grid(row=1, column=1, rowspan=2, sticky="nsew")
+# Crear el Listbox en la segunda fila
+lista_archivos = tk.Listbox(ventana)
+lista_archivos.grid(row=1, column=0, sticky="nsew")
 
 # Configurar el tamaño del Listbox para ocupar más espacio
-lista_directorios.rowconfigure(0, weight=1)
-lista_directorios.columnconfigure(0, weight=1)
+lista_archivos.rowconfigure(0, weight=1)
+lista_archivos.columnconfigure(0, weight=1)
 
 # Ejecutar el bucle principal de la ventana
 ventana.mainloop()
